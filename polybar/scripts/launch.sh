bd230aabde987f4951013e471f72af3bd8a29bca
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2022-08-06 19:08:50 +0800

    Change script to run polybar on multiple screens

--- polybar/scripts/launch.sh
+++ polybar/scripts/launch.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-DIR="/etc/polybar"
+# DIR="/etc/polybar"
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q main -c "$DIR"/config.ini &
-#polybar -q main2 -c "$DIR"/config.ini &
+# install autorandr first before run this script to set polybar on multiple screens
+# polybar -q main -c "$DIR"/config.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
 
