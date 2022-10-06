6bb42d40835caa97fae6c7c5b49c9301743ab6b3
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2022-10-06 19:52:07 +0800

    Add lsp for go

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-  local servers = { "jsonls", "sumneko_lua", "tsserver", "dockerls","rust_analyzer" }
+  local servers = { "jsonls", "sumneko_lua", "tsserver", "dockerls","rust_analyzer","golangci_lint_ls","gopls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
