41a9d75b189154b6c3b7af52e80076f8ae53fd75
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2022-10-17 22:15:30 +0800

    Remove neotree

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -46,61 +46,14 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
 	use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
 	use "numToStr/Comment.nvim" -- Comment
---[[  use { ]]
---[[   'nvim-tree/nvim-tree.lua', ]]
---[[   requires = { ]]
---[[     'nvim-tree/nvim-web-devicons', -- optional, for file icons ]]
---[[   }, ]]
---[[   tag = 'nightly' -- optional, updated every week. (see issue #1193) ]]
---[[ } ]]
-use {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      {
-        -- only needed if you want to use the commands with "_with_window_picker" suffix
-        's1n7ax/nvim-window-picker',
-        tag = "v1.*",
-        config = function()
-          require'window-picker'.setup({
-            autoselect_one = true,
-            include_current = false,
-            filter_rules = {
-              -- filter using buffer options
-              bo = {
-                -- if the file type is one of following, the window will be ignored
-                filetype = { 'neo-tree', "neo-tree-popup", "notify", "quickfix" },
-
-                -- if the buffer type is one of following, the window will be ignored
-                buftype = { 'terminal' },
-              },
-            },
-            other_win_hl_color = '#e35e4f',
-          })
-        end,
-      }
-    },
-    config = function ()
-      -- Unless you are still migrating, remove the deprecated commands from v1.x
-      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
-      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-      vim.fn.sign_define("DiagnosticSignError",
-        {text = " ", texthl = "DiagnosticSignError"})
-      vim.fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
-      vim.fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
-      vim.fn.sign_define("DiagnosticSignHint",
-        {text = "", texthl = "DiagnosticSignHint"})
-      -- NOTE: this is changed from v1.x, which used the old style of highlight groups
-      -- in the form "LspDiagnosticsSignWarning"
-      vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-    end
-}  use "JoosepAlviste/nvim-ts-context-commentstring" -- Comment string
+ use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+  use "JoosepAlviste/nvim-ts-context-commentstring" -- Comment string
   --[[ use "kyazdani42/nvim-web-devicons" -- icons ]]
   --[[ use "kyazdani42/nvim-tree.lua" -- nvim tree ]]
   use "moll/vim-bbye"
