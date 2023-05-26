d549c854348cf0f340a841a84b1a0f73dc337e41
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-05-26 19:28:00 +0800

    Add preview treesitter false

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -12,7 +12,7 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
-
+    preview = { treesitter = false },
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
