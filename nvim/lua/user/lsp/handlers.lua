ecf06aa10819f58a075c0bb30600373104531a76
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2024-05-06 10:51:57 +0800

    Fix handlers updated

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -15,13 +15,13 @@ M.setup = function()
 
 	local config = {
 		-- disable virtual text
-		virtual_text = false,
+		virtual_text = true, -- error or warning signs
 		-- show signs
 		signs = {
 			active = signs,
 		},
 		update_in_insert = true,
-		underline = true,
+		underline = false,
 		severity_sort = true,
 		float = {
 			focusable = false,
@@ -36,12 +36,11 @@ M.setup = function()
 	vim.diagnostic.config(config)
 
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded"
+		border = "rounded",
 	})
 
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 		border = "rounded",
-		
 	})
 end
 
@@ -49,34 +48,35 @@ local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
 	--local status_ok, illuminate = pcall(require, "illuminate")
 	--if not status_ok then
-		--return
+	--return
 	--end
 	--illuminate.on_attach(client)
 	-- end
- if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
+	if client.server_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
-      false
-    )
-  end
+			false
+		)
+	end
 end
 
 local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  --[[ vim.api.nvim_buf_set_keymap(bufnr,"n","K",  "<cmd>Lspsaga hover_doc<CR>", opts) ]]
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(
@@ -88,15 +88,32 @@ local function lsp_keymaps(bufnr)
 	)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+	-- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format({ timeout_ms = 2000 })' ]]
 end
 
 M.on_attach = function(client, bufnr)
+local twoslash_queries_status_ok, twoslash_queries= pcall(require, "twoslash-queries")
+
+if not  twoslash_queries_status_ok then
+  return
+end
 	-- vim.notify(client.name .. " starting...")
 	-- TODO: refactor this into a method that checks if string in list
-	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
-	end
+  if client.name == "tsserver" then
+    client.server_capabilities.documentFormattingProvider = true
+    twoslash_queries.attach(client,bufnr)
+  end
+
+  if client.name == "eslint" then
+    client.server_capabilities.documentFormattingProvider = true
+  end
+    if client.server_capabilities.documentRangeFormattingProvider then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
+  -- if client.name == "rust-analyzer" then
+  --   client.resolved_capabilities.document_formatting = false
+  -- end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
 end
@@ -108,6 +125,6 @@ if not status_ok then
 	return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
